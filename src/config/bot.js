4bdd185841a21a58d6b5529c26aafe16fc04f61e
bingpf <misaosovskij4@gmail.com>
2026-06-14 19:45:00 +0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Камиль, ждем открытия 20 сервера)",
         // Activity type number (0 = Playing).
         type: 0, 
       },
