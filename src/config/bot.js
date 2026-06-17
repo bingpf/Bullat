ab302f50d40ab8b9deb1a24a886a02e72b222e54
bingpf <misaosovskij4@gmail.com>
2026-06-17 13:06:53 +0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Bullat loves Raniel",
+        name: "Bullat loves Raniel, loves Ivan (Shon)",
         // Activity type number (0 = Playing).
         type: 0, 
       },
